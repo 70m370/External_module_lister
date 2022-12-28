01a296c5aaef9ebf2b4cbe574bd67b62342cacff
Gabriel <grnt1648@gmail.com>
2022-12-27 22:23:59 -0300

    Final Commit
    Garbage code removed, and uptade on functions to work better, with if statements that will make a better working flow into the program

--- main.cpp
+++ main.cpp
@@ -4,20 +4,24 @@
 #include<iostream>
 #include<string>
 
+
+
 //
 //Project IDEA so far
-
-// - CHECK 1
+ 
+// - CHECK 1 DONE
 //receive user input(executable name) - search for it - find the process ID
 
-// - CHECK 2
+// - CHECK 2 DONE
 //run the printmodules, list the modules inside the application
 
-// - DO
+// - DO DONE
 //ask for the specified module name to list what its loading in memory
+//solved with module number
 
-//
+// DONE
 //create a static menu where you restart the search, Getasynckeystate - something to restart the app
+//solved with goto statemant way easier but unsafe in case of bufferoverflow, i thought about using assembly directly into it
 
 //
 //Macros
@@ -78,7 +82,7 @@ int PrintModules(DWORD processID)
 
 	// Get a handle to the process.
 
-	hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processID);
+	hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processID);
 	if (NULL == hProcess)
 		return 1;
 
@@ -94,8 +98,7 @@ int PrintModules(DWORD processID)
 
 			if (GetModuleFileNameEx(hProcess, hMods[i], szModName, sizeof(szModName) / sizeof(TCHAR)))
 			{
-
-				// Print the module name and handle value.
+				// counter for modules inside
 				modulesinapp++;
 			}
 		}
@@ -108,29 +111,7 @@ int PrintModules(DWORD processID)
 	return 0;
 }
 
-uintptr_t GetModuleBaseAddress(DWORD procId, char* modName) // uintptr_t architecture independet variable - usable in any architechure - will run in x86 x64
-{//module base address is going to be the function that acces the memory variables insite the procID
-	uintptr_t modBaseAddr = 0; //unsigned int pointer
-	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, procId); //TH32CS_SNAPMODULE includes all modules of the process specified in th32ProcessID in the snapshot. 
-	if (hSnap != INVALID_HANDLE_VALUE)
-	{
-		MODULEENTRY32 modEntry; //MODULEENTRY32 Describes an entry from a list of the modules belonging to the specified process.
-		modEntry.dwSize = sizeof(modEntry);// using the size refference to module
-		if (Module32First(hSnap, &modEntry))
-		{
-			do
-			{
-				if (!strcmp(modEntry.szExePath, modName))//.szModule = module name
-				{
-					modBaseAddr = (uintptr_t)modEntry.modBaseAddr; //The base address of the module in the context of the owning process.
-					break;
-				}
-			} while (Module32Next(hSnap, &modEntry));
-		}
-	}
-	CloseHandle(hSnap);
-	return modBaseAddr;
-}
+
 
 //Get all module related info, this will include the base DLL.
 //and the size of the module
@@ -143,9 +124,10 @@ MODULEINFO GetModuleInfo(HMODULE hModule)
 }
 
 //
-//the problem is inside here, something with the virtualquery- found the problem - 
+//function that i adapted from msdn
 int ModuleLister(int module_number_input)
-{
+{	
+
 	MODULEINFO mInfoinside = GetModuleInfo(hMods[module_number_input]);
 	DWORD baseinside = (DWORD)mInfoinside.lpBaseOfDll;
 	DWORD sizeinside = (DWORD)mInfoinside.SizeOfImage;
@@ -153,67 +135,52 @@ int ModuleLister(int module_number_input)
 
 	int i = 0;
 
-	HANDLE curproc = OpenProcess(PROCESS_QUERY_INFORMATION |
-		PROCESS_VM_READ,
+	HANDLE curproc = OpenProcess(PROCESS_ALL_ACCESS,
 		FALSE, procID); //used to be FALSE - testing right now
 
 	TCHAR szModName[MAX_PATH];
 	DWORD nbuffer;
+
 	//
 	// Get the full path to the module's file.
 	GetModuleFileNameEx(curproc, hMods[module_number_input], szModName, sizeof(szModName) / sizeof(TCHAR));
-	//moduleBase = GetModuleBaseAddress(procId, L"ac_client.exe");
+	
 
 	while (i <= sizeinside)
 	{
 		int num = 400000;
 		i++;
 		int valuehex = 0x0 + i;
-		DWORD* BaseModuleAddresses = (DWORD*)(hMods[module_number_input] + valuehex);
-		//unsigned long long* BaseModuleAddresses = (unsigned long long*)(ac_module + valuehex);
-		DWORD oldprotect;
-		VirtualProtectEx(process, (PVOID)BaseModuleAddresses,sizeof(BaseModuleAddresses), PAGE_EXECUTE_READ,&oldprotect);
-		ReadProcessMemory(process, (PVOID)BaseModuleAddresses, &nbuffer, sizeof(BaseModuleAddresses), 0);
-		
+		DWORD* BaseModuleAddresses = (DWORD*)(baseinside + valuehex);
+	
 		//
 		//Retrieving information about a range of pages within the virtual address space.
 		MEMORY_BASIC_INFORMATION memory_info;
-		VirtualQuery((LPCVOID)BaseModuleAddresses, &memory_info, sizeof(memory_info));
-		//
-		//the problem is the exported file - its not receiving all the info gathered
-		//
-		//Checking if the memory region inside the page is free
-		//if (memory_info.State & MEM_FREE)
-		//{
-		//	printf("ERROR CODE 0x%08x", GetLastError());
-		//	break;
-		//}
-		//
-		//Checking the page permission to proceed
+		VirtualQueryEx(curproc,(LPCVOID)BaseModuleAddresses, &memory_info, sizeof(memory_info));
+
 		if (memory_info.Protect & MEMO_PROTEC)
 		{
-			DWORD* BaseModuleAddressesValues = (DWORD*)(*BaseModuleAddresses);
-			//
-			//checking if the Addresses != NULL
-			if (BaseModuleAddressesValues)
+			if (BaseModuleAddresses)
 			{
-				//
-				//exporting dump content to inside File
-				//textdump << szModName << " + " << std::hex << BaseModuleAddresses << " Acess: " << std::hex << BaseModuleAddressesValues << "\n";
-				//fprintf(fptr, "%s  +  %x  Access:  %x \t%d\n",szModName,BaseModuleAddresses,BaseModuleAddressesValues,simple_counter);
-				std::cout << szModName << " + " << std::hex << BaseModuleAddresses << " Acess: " << std::hex << BaseModuleAddressesValues << "\n";
-				//modules_i_can_read++;
+				ReadProcessMemory(curproc, (PVOID)BaseModuleAddresses, &nbuffer, sizeof(BaseModuleAddresses), 0);
+				std::cout << szModName << " (" << std::hex << BaseModuleAddresses << ") " << " + " << valuehex << " Acess: " << std::hex << nbuffer << "\n"; // it worked
+			}
+			else
+			{
+				std::cout << szModName << " + " << std::hex << BaseModuleAddresses << " NULL" << "\n";
 			}
 		}
-		VirtualProtectEx(process, (PVOID)BaseModuleAddresses, sizeof(BaseModuleAddresses), oldprotect, &oldprotect);
 	}
-
+	
 	return 0;
 }
 
 
 int main()
 {
+	//setting jump place
+	startpoint:
+
 	//
 	//part that will treat the input and turn it into the otput needed to search for the process 
 	std::cout << "Insert the name of the executable you want to open :> ";
@@ -221,8 +188,8 @@ int main()
 	std::cin >> procname;
 	char* processname = const_cast<char*>(procname.c_str());
 
-	getprocess(processname);
 
+	getprocess(processname);
 	PrintModules(procID);
 
 	printf("%d modules inside this executable \n", modulesinapp);
@@ -230,13 +197,12 @@ int main()
 
 	int y = 0;
 	int module_number = 0;
-	//DWORD ModulesAddreessess[MAX_PATH];
+
 	while (y <= modulesinapp)
 	{
 		y++;
 		module_number++;
-		//
-		//testing only ac_client.exe - everything worked fine - the problem is in fstream / exporting to .txt
+
 		MODULEINFO mInfoinside = GetModuleInfo(hMods[y]);
 		DWORD baseinside = (DWORD)mInfoinside.lpBaseOfDll;
 		DWORD sizeinside = (DWORD)mInfoinside.SizeOfImage;
@@ -246,39 +212,42 @@ int main()
 		// Opening the process with permitions  specified and storing this address into a handle
 		HANDLE curproc = OpenProcess(PROCESS_QUERY_INFORMATION |
 			PROCESS_VM_READ,
-			FALSE, procID); //used to be FALSE - testing right now
+			FALSE, procID); 
 
 		TCHAR szModName[MAX_PATH];
 		
 		//
 		// Get the full path to the module's file.
 		GetModuleFileNameEx(curproc, hMods[y], szModName, sizeof(szModName) / sizeof(TCHAR));
-		//ModulesAddreessess[y] = GetModuleBaseAddress(procID, (char*)(szModName[y]));
-		//
-		//printing modules path inside application and their size.          
-		printf(TEXT("\t%s (0x%08X)\t Module number: %d\n"), szModName, hMods[y], module_number);
-		
-		
+		if (baseinside != 0)
+		{
+			printf(TEXT("\t%s (0x%08X)\t Module number: %d\n"), szModName, hMods[y], module_number);
+		}	
 	}
 
+	std::cout << "\n insert the module number of the module you want to list: ";
 	int modulenumbersearch = 0;
 
 	std::cin >> modulenumbersearch;
 
-	ModuleLister(modulenumbersearch);//problem inside here finding the correct place in memory where the module is stored
+	ModuleLister(modulenumbersearch);
 
 	Sleep(4);
-	//const wchar_t* szName = name.c_str();
-	//printf("%c", procname);
+	std::cout << "\n" << "Would you like to make another search ? (Y/N) : ";
+	std::string decision;
+	std::cin >> decision;
+	if (decision == "y")
+	{
+		process = NULL;
+		procID = 0;
+		hMods[1024] = 0;
+		goto startpoint;
+	}
+	else
+	{
+		std::cout << "\n";
+		return 0;
+	}
 	std::cout << "\n";
-	//GetProcId(finalexecname);
-	//PrintModules();
-	int test = 0;
-	std::cin >> test;
-	// free((void*)finalexecname);
 	return 0;
 }
-
-//	Idea to solve this problem, export the listing functions to a dll to execute it inside, inject the dll and then use the dll functions through the Loadlibraryex
-
-//	07/11 - I tried to use readprocessmemory, dont got any luck - changed the memory protection nothing - new function - check Github to see the changes 
